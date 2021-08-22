c89b44896c154e4b2f45655e05bd93803779871d
Mohamed M. Mansour <mohamed_mahmoud96357@yahoo.com>
2021-08-22 02:46:55 +0200

    added search icon, background and details of the SideMenu

--- lib/ui/HomeScreen.dart
+++ lib/ui/HomeScreen.dart
@@ -1,22 +1,111 @@
 import 'package:flutter/material.dart';
 import 'package:news_flutter/ui/SideMenu.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  bool _isTextFieldActive = false;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: new Text('',/**to add Category name**/),
-        centerTitle: true,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.only(
-            bottomLeft: Radius.circular(28), bottomRight: Radius.circular(28),
-          )
-        ) ,
+    var searchController = TextEditingController() ;
+    return GestureDetector(
+      onTap: () =>FocusScope.of(context).requestFocus(new FocusNode()),
+      child: Scaffold(
+        appBar: !_isTextFieldActive ? AppBar(
+          title: new Text('',/**to add Category name**/),
+          centerTitle: true,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(28), bottomRight: Radius.circular(28),
+            )
+          ),
+          flexibleSpace: Row(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Expanded(
+                child: Align(
+                    alignment: Alignment.centerRight,
+                    child: IconButton(
+                      padding: EdgeInsets.only(top: 25, right: 25),
+                      color: Colors.white,
+                      iconSize: 34,
+                      icon: Icon(Icons.search),
+                      onPressed: (){
+                        setState(() {
+                          _isTextFieldActive=true;
+                        });
+                      },
+                    ),
+                ),
+              ),
+            ],
+          ),
+        )
+        : AppBar(
+          automaticallyImplyLeading: false,
+          actionsIconTheme: IconThemeData(
+            color: Theme.of(context).primaryColor,
+          ),
+          shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(28), bottomRight: Radius.circular(28),
+              )
+          ),
+          actions: [
+            Expanded(
+              child: FractionallySizedBox(
+                alignment: Alignment.center,
+                widthFactor: 0.83,
+                heightFactor: 0.72,
+                child: new Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(35),
+                    color: Colors.white,
+                  ),
+                  child: TextField(
+                      style: TextStyle(color: Theme.of(context).primaryColor),
+                      controller: searchController = TextEditingController() ,
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        prefixIcon: IconButton(
+                          color: Theme.of(context).primaryColor,
+                          icon: Icon(Icons.close),
+                          onPressed: (){
+                            setState(() {
+                              _isTextFieldActive=false;
+                            });
+                          },
+                        ),
+                        suffixIcon: IconButton(
+                            color: Theme.of(context).primaryColor,
+                            icon: Icon(Icons.search),
+                            onPressed: (){},
+                        ),
+                        contentPadding: EdgeInsets.only(top: 6),
+                        hintText: 'Search Article',
+                        hintStyle: TextStyle(fontSize: 13, color: Color.fromRGBO(57, 165, 82, .35), fontWeight: FontWeight.bold),
+                      )
+                    ),
+                  ),
+                ),
+              ),
+            ]
+        ),
 
+        drawer: Drawer(
+          child: AppSideMenu(),
+        ),
+        body: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(image: AssetImage("assets/bckImg.png"), fit: BoxFit.cover,),
+          ),
+          child: Container(),///to add body from API
+        )
       ),
-      drawer: AppSideMenu(),
-      body: Container(),  ///to add body from API
     );
   }
 }
--- lib/ui/SideMenu.dart
+++ lib/ui/SideMenu.dart
@@ -1,9 +1,62 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class AppSideMenu extends StatelessWidget {
+  List<SideMenuItemData> sideMenuList =[
+    SideMenuItemData(Icons.view_list_rounded, "Categoriese"),
+    SideMenuItemData(Icons.settings, "Settings"),
+
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: new Column(
+        children: [
+          new Container(
+            color: Theme.of(context).primaryColor,
+            padding: EdgeInsets.symmetric(vertical: 55),
+            child: new Center(
+              child: new Text('News App!',
+                style: new TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 21),
+              ),
+            )
+          ),
+          Expanded(
+              child: ListView.builder(itemBuilder: (context, index)
+                  {
+                    return SideMenuItem(sideMenuList[index]);
+                  },
+                  itemCount: sideMenuList.length,
+              )
+          )
+
+        ],
+      ),
+    );
+  }
+}
+
+class SideMenuItemData
+{
+  IconData iconData;
+  String title;
+  SideMenuItemData(this.iconData, this.title);
+}
+
+class SideMenuItem extends StatelessWidget{
+  SideMenuItemData myData;
+  SideMenuItem(this.myData);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        Padding(padding: EdgeInsets.all(16), child: Icon(myData.iconData, size: 26,),),
+        Text(myData.title, style: TextStyle(fontWeight: FontWeight.bold, fontSize: 21),),
+      ],
+    );
   }
+
 }
+
